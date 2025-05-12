49b11d5ab4e0e52a19c68be4c26fa4bf88c86434
Emanuelly Valenga <emanuellvalenga.dev@gmail.com>
2025-05-12 17:31:41 -0300

    Criado a model de Employee (vazia)

--- api-employees/app/Models/Employee.php
+++ api-employees/app/Models/Employee.php
@@ -4,7 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static insert(array $arrayValues)
+ * @method static where(string $string, mixed $key)
+ */
 class Employee extends Model
 {
-    //
 }
