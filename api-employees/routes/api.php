907e02e919a063218c23415239510382298184f7
Emanuelly Valenga <emanuellvalenga.dev@gmail.com>
2025-05-15 15:19:17 -0300

    Realizado correções nas rotas protegidas

--- api-employees/routes/api.php
+++ api-employees/routes/api.php
@@ -5,16 +5,19 @@ use App\Http\Controllers\Api\EmployeeController;
 use App\Http\Controllers\Api\ManagerController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:manager')->group(function () {
-    Route::apiResource('employees', EmployeeController::class);
-    Route::post('/employees/import', [EmployeeController::class, 'import']);
-});
-
-// Public routes
+// Rota pública
 Route::post('/manager/login', [AuthController::class, 'login']);
 
-// Protected routes -> auth:manager
+// Rotas protegidas com acesso autenticado do gestor
 Route::middleware('auth:manager')->group(function () {
+    // Gestor (Manager)
     Route::apiResource('managers', ManagerController::class);
+
+    // Autenticação (Auth)
     Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+
+    // Colaboradores (Employees)
+    Route::apiResource('employees', EmployeeController::class);
+    Route::post('/employees/import', [EmployeeController::class, 'import']);
 });
