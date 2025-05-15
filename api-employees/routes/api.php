9e9a1768755e1757c43082785c1be51ac36775f7
Emanuelly Valenga <emanuellyvalenga.dev@gmail.com>
2025-05-14 21:25:06 -0300

    Refinando rotas com middleware e apiResource

--- api-employees/routes/api.php
+++ api-employees/routes/api.php
@@ -1,27 +1,25 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EmployeeController;
 use App\Http\Controllers\Api\ManagerController;
 use Illuminate\Support\Facades\Route;
 
-// GET - http://127.0.0.1:8000/api/employees
-Route::get('/employees', [EmployeeController::class, 'index']);
-// GET - http://127.0.0.1:8000/api/employees/{id}
-Route::get('/employees/{employee}', [EmployeeController::class, 'show']);
-// POST - http://127.0.0.1:8000/api/employees/
-Route::post('/employees', [EmployeeController::class, 'store']);
-// PUT - http://127.0.0.1:8000/api/employee/{id}
-Route::put('/employees/{employee}', [EmployeeController::class, 'update']);
-// DELETE - http://127.0.0.1:8000/api/employees/{id}
-Route::delete('/employees/{employee}', [EmployeeController::class, 'destroy']);
+//Route::get('/employees', [EmployeeController::class, 'index']);
+//Route::get('/employees/{employee}', [EmployeeController::class, 'show']);
+//Route::post('/employees', [EmployeeController::class, 'store']);
+//Route::put('/employees/{employee}', [EmployeeController::class, 'update']);
+//Route::delete('/employees/{employee}', [EmployeeController::class, 'destroy']);
 
-// GET - http://127.0.0.1:8000/api/managers
-Route::get('/managers', [ManagerController::class, 'index']);
-// GET - http://127.0.0.1:8000/api/managers/{id}
-Route::get('/managers/{manager}', [ManagerController::class, 'show']);
-// POST - http://127.0.0.1:8000/api/managers/
-Route::post('/managers', [ManagerController::class, 'store']);
-// PUT - http://127.0.0.1:8000/api/managers/{id}
-Route::put('/managers/{manager}', [ManagerController::class, 'update']);
-// DELETE - http://127.0.0.1:8000/api/managers/{id}
-Route::delete('/managers/{manager}', [ManagerController::class, 'destroy']);
+Route::middleware('auth:manager')->group(function () {
+    Route::apiResource('employees', EmployeeController::class);
+});
+
+// Public routes
+Route::post('/manager/login', [AuthController::class, 'login']);
+
+// Protected routes -> auth:manager
+Route::middleware('auth:manager')->group(function () {
+    Route::apiResource('managers', ManagerController::class);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
