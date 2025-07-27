c4749daf87652a654cf5760708d61dd3e76efcf2
Emanuelly Valenga <emanuellvalenga.dev@gmail.com>
2025-07-26 21:58:26 -0300

    Iniciando estudos sobre testes

---
+++ estudos-sobre-testes/php-e-tdd-testes-com-phpunit/src/Model/Leilao.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Alura\Leilao\Model;
+
+class Leilao
+{
+    /** @var Lance[] */
+    private array $lances;
+    /** @var string */
+    private string $descricao;
+
+    public function __construct(string $descricao)
+    {
+        $this->descricao = $descricao;
+        $this->lances = [];
+    }
+
+    public function recebeLance(Lance $lance): void
+    {
+        $this->lances[] = $lance;
+    }
+
+    /**
+     * @return Lance[]
+     */
+    public function getLances(): array
+    {
+        return $this->lances;
+    }
+}
